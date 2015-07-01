c055fab718f2fcf9e24aac5b8c160ce95578df64
Mengchen Zhu <mengchen.zhu@gmail.com>
2015-07-01 13:19:19 -0400

    almost ridding all global vars

--- app/views.py
+++ app/views.py
@@ -10,12 +10,9 @@ import pdb
 @app.route('/index')
 def index():
     jobDescription = request.args.get('jobDescription')
-    # global checked
     session['checked'] = request.args.get('checked')
     global rocchio
-    global jobInd
-    global sameCompany
-    sameCompany = []
+    session['sameCompany'] = []
 
     if jobDescription is None:
         jobsList = []
@@ -23,18 +20,16 @@ def index():
         sim_score_sorted = ""
         session['checked'] = "checked"
     else:
-        # jobs = Jobs()
         rocchio = Rocchio(app.jobs.getVecRepMat(), app.jobs.getVecRep(jobDescription))
         simInd, sim_score = app.jobs.findSimilar(jobDescription)
         sim_score_sorted = [sim_score[i] for i in simInd]
         jobsList = [app.jobs.getJob(ind) for ind in simInd]
-        jobInd = simInd[0]
+        session['jobInd'] = simInd[0]
     return render_template("index.html", jobsList = jobsList, jobDescription = jobDescription, sim_score = sim_score_sorted, checked = session['checked'])
 
 @app.route('/search')
 def search():
     jobDescription = request.args.get('jobDescription')
-    # TODO: instead of global variables, use flask session and define own JSON conversion
 
     if jobDescription is None:
         jobsList = []
@@ -49,29 +44,21 @@ def search():
 
 @app.route('/like')
 def like():
-    global jobInd
     global rocchio
-    global sameCompany
-    newQuery = rocchio.addToRelevant(jobInd)
-    # global checked
-    # checked = request.args.get('checked')
+    newQuery = rocchio.addToRelevant(session['jobInd'])
     if session['checked']:
-        sameCompany = sameCompany + app.jobs.findJobsInSameCompany(jobInd)
-    topInd, simVal = app.jobs.findSimilarFromVec(newQuery, top=1, exclude=rocchio.getRelevant()+rocchio.getIrrelevant()+sameCompany)
-    jobInd = topInd[0]
-    return render_template("like.html", topJob=app.jobs.getJob(jobInd))
+        session['sameCompany'] = session['sameCompany'] + app.jobs.findJobsInSameCompany(session['jobInd'])
+    topInd, simVal = app.jobs.findSimilarFromVec(newQuery, top=1, exclude=rocchio.getRelevant()+rocchio.getIrrelevant()+session['sameCompany'])
+    session['jobInd'] = topInd[0]
+    return render_template("like.html", topJob=app.jobs.getJob(session['jobInd']))
 
 @app.route('/dislike')
 def dislike():
-    global jobInd
     global rocchio
-    global sameCompany
-    # global checked
-    # checked = request.args.get('checked')
-    newQuery = rocchio.addToIrrelevant(jobInd)
-    topInd, simVal = app.jobs.findSimilarFromVec(newQuery, top=1, exclude=rocchio.getRelevant()+rocchio.getIrrelevant()+sameCompany)
-    jobInd = topInd[0]
-    return render_template("dislike.html", topJob=app.jobs.getJob(jobInd))
+    newQuery = rocchio.addToIrrelevant(session['jobInd'])
+    topInd, simVal = app.jobs.findSimilarFromVec(newQuery, top=1, exclude=rocchio.getRelevant()+rocchio.getIrrelevant()+session['sameCompany'])
+    session['jobInd'] = topInd[0]
+    return render_template("dislike.html", topJob=app.jobs.getJob(session['jobInd']))
     
 @app.route('/viewliked')
 def viewliked():
